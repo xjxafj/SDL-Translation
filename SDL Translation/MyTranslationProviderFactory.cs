1c397693374c74c55e2c730ca6461b473b273488
fujia <xjxafj@126.com>
2019-02-19 22:20:49 +0800

    添加项目文件。

---
+++ SDL Translation/MyTranslationProviderFactory.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Sdl.LanguagePlatform.Core;
+using Sdl.LanguagePlatform.TranslationMemory;
+using Sdl.LanguagePlatform.TranslationMemoryApi;
+
+namespace SDL_Translation
+{
+    [TranslationProviderFactory(Id = "Translation_Provider_Plug_inFactory",
+                                Name = "Translation_Provider_Plug_inFactory",
+                                Description = "Translation_Provider_Plug_inFactory")]
+    class MyTranslationProviderFactory : ITranslationProviderFactory
+    {
+        #region ITranslationProviderFactory Members
+
+        public ITranslationProvider CreateTranslationProvider(Uri translationProviderUri, string translationProviderState, ITranslationProviderCredentialStore credentialStore)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TranslationProviderInfo GetTranslationProviderInfo(Uri translationProviderUri, string translationProviderState)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool SupportsTranslationProviderUri(Uri translationProviderUri)
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
